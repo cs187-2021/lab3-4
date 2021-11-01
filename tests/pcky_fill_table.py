53c7f465c83d17814bd1e614c66f827d2afedd4a
cs187staff <dengyuntian@g.harvard.edu>
2021-11-01 23:15:37 +0000

    automatic update

--- tests/pcky_fill_table.py
+++ tests/pcky_fill_table.py
@@ -1,7 +1,1 @@
-test = {   'name': 'pcky_fill_table',
-    'points': 1,
-    'suites': [   {   'cases': [{'code': ">>> table.iloc[4, 5]['S'] - 0.35 * table.iloc[4, 5]['NUM'] < 0.0001\nTrue", 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
+test = {'name': 'pcky_fill_table', 'points': 6, 'suites': [{'cases': [], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
